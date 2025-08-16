99d76250dc19ee754e60f3b33d7f0bc3e709f8da
Chrismar Ilustrisimo <chrismarilustrisimo9@gmail.com>
2025-08-17 01:53:29 +0800

    feat: implement token validation in Feed component to redirect unauthenticated users

--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -1,15 +1,23 @@
-
 import Navbar from "../components/Navbar"
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Feed = () => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, [token, navigate]);  // runs once when component mounts
 
   return (
     <div>
-        <Navbar />
-
+      <Navbar />
+      FEED
     </div>
-  )
-}
+  );
+};
 
-export default Feed 
+export default Feed;
